fe5ac5e3c8d71a9b64e7903d6698526f13b8ef11
vimal8086 <86038901+vimal8086@users.noreply.github.com>
2025-03-06 13:56:55 +0530

    Update AuthController.java
    Initial Commit (API For Auth, Route, Destinaion)

--- src/main/java/com/one/digitalapi/controller/AuthController.java
+++ src/main/java/com/one/digitalapi/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
