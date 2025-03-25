d905290b15a47d859ac4b49b455e1255403cd6db
vimal8086 <86038901+vimal8086@users.noreply.github.com>
2025-03-25 15:49:55 +0530

    Validation

--- src/main/java/com/one/digitalapi/entity/User.java
+++ src/main/java/com/one/digitalapi/entity/User.java
@@ -3,6 +3,7 @@ package com.one.digitalapi.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -26,11 +27,13 @@ public class User {
     private String userId;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @NotBlank(message = "code is required")
     private String code;
 
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     @Column(unique = true, nullable = false)
     private String email;
